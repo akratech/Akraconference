ad068af8605271cdc565c4d44a984105e4c2d8cd
Arvind Mehra <bloomberg490@gmail.com>
2016-09-09 19:25:05 +0530

    send email to admin@atconference.online for queries

--- app/mailers/query_mailer.rb
+++ app/mailers/query_mailer.rb
@@ -4,7 +4,7 @@ class QueryMailer < ActionMailer::Base
 	def get_in_touch(name,email,number)
 	    subject = "Query from #{name}"
 	    @body = "Query from #{name} phone number: #{number}"
-	    mail(:to => "bloomberg490@gmail.com",:subject => subject,:from => email)
+	    mail(:to => "admin@atconference.online",:subject => subject,:from => email)
   end
 
 end
