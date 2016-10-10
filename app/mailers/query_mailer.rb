c933b39435a5628063b110c771e6ceff5accb671
Arvind Mehra <bloomberg490@gmail.com>
2016-10-10 22:24:24 +0530

    changed email address from contact@akra-tech to admin@atconference

--- app/mailers/query_mailer.rb
+++ app/mailers/query_mailer.rb
@@ -2,14 +2,14 @@ require 'net/http'
 class QueryMailer < ActionMailer::Base
 
   def get_in_touch(name,email,number,message)
-    email_id = Rails.env.production? ? "contact@akra-tech.com" : "bloomberg490@gmail.com"
+    email_id = Rails.env.production? ? "admin@atconference.online" : "bloomberg490@gmail.com"
     subject = "Query from #{name} phone number: #{number}"
     @body = "#{message}"
     mail(:to => email_id,:subject => subject,:from => email)
   end
 
   def contact_us(name,email,subject,message)
-    email_id = Rails.env.production? ? "contact@akra-tech.com" : "bloomberg490@gmail.com"
+    email_id = Rails.env.production? ? "admin@atconference.online" : "bloomberg490@gmail.com"
     subject = subject
     @body = "#{message}"
     mail(:to => email_id,:subject => subject,:from => email)
